306bf10373e03391d5c2345c8fca97d3717a6308
YP5ka <ypyka12@gmail.com>
2024-04-05 00:26:23 +0300

    еще микро-фиксы

--- src/components/pages/MainPage.tsx
+++ src/components/pages/MainPage.tsx
@@ -25,35 +25,35 @@ const MainPage = () => {
             </div>
         </div>
 
-        <div className="flex flex-col items-center mt-32 px-6">
+        <div className="flex flex-col items-center mt-32 px-2">
             <h1 className="sm:text-5xl text-3xl font-semibold  text-center">Мои юридические услуги</h1>
                 <div className="flex min-[1240px]:flex-row mt-20 flex-col">
-                    <div className="sm:mr-8 cursor-pointer mb-12">
-                        <img className=" max-w-sm rounded-md px-6" src="/images/house_1.jpg"/>
-                        <figcaption className="mt-2 max-w-sm text-start text-2xl font-bold px-6">Решение имущественных вопросов</figcaption>
+                    <div className="sm:mr-8 cursor-pointer mb-12 ">
+                        <img className="  rounded-md px-6" src="/images/house_1.jpg"/>
+                        <figcaption className="mt-2  text-start text-2xl font-bold px-6">Решение имущественных вопросов</figcaption>
                     </div>
                     <div className="sm:mr-8 cursor-pointer mb-12">
-                        <img  className=" max-w-sm rounded-md px-6 " src="/images/family_2.jpg"/>
+                        <img  className="  rounded-md px-6 " src="/images/family_2.jpg"/>
                         <figcaption className="max-w-xs mt-2 text-2xl font-bold text-start px-6">Решение семейных вопросов</figcaption>
                     </div>
                     <div className="cursor-pointer mb-12">
-                        <img  className=" max-w-sm rounded-md px-6" src="/images/molot_3.jpg"/>
+                        <img  className=" rounded-md px-6" src="/images/molot_3.jpg"/>
                         <figcaption className=" max-w-xs mt-2 text-2xl font-bold text-start px-6">Решение земельных вопросов</figcaption>
                     </div>
                 </div>
                 
                 <div className="flex  min-[1240px]:flex-row min-[1240px]:mt-12 flex-col">
                     <div className="sm:mr-8 cursor-pointe mb-12">
-                        <img className=" max-w-sm rounded-md px-6" src="/images/key_4.jpg"/>
+                        <img className="  rounded-md px-6" src="/images/key_4.jpg"/>
                         <figcaption className="mt-2 max-w-xs text-start text-2xl font-bold px-6">Решение квартирных вопросов</figcaption>
                     </div>
                     <div className="sm:mr-8 cursor-pointer mb-12">
-                        <img  className=" max-w-sm rounded-md px-6" src="/images/write_5.jpeg"/>
+                        <img  className="  rounded-md px-6" src="/images/write_5.jpeg"/>
                         <figcaption className="max-w-xs mt-2 text-2xl font-bold text-start px-6">Решение банкротных вопросов</figcaption>
                     </div>
                     <div className="cursor-pointer mb-12">
-                        <img  className=" max-w-sm rounded-md px-6" src="/images/balance_6.jpeg"/>
-                        <figcaption className="max-w-sm mt-2 text-2xl font-bold text-start px-6">Решение особых юридических вопросов</figcaption>
+                        <img  className=" rounded-md px-6" src="/images/balance_6.jpeg"/>
+                        <figcaption className=" mt-2 text-2xl font-bold text-start px-6">Решение особых юридических вопросов</figcaption>
                     </div>
                 </div>
         </div>
